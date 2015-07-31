51487c23868095c92fd926dabab33a95da2e92bf
taery <lavrenko.a@gmail.com>
2015-07-31 18:02:42 +0300

    use elasticutils for searching

--- searchengine/searchclient.py
+++ searchengine/searchclient.py
@@ -1,26 +1,7 @@
-import json
-import requests
+from elasticutils import S
+from searchengine.models import LessonMapping
 
 
 def search(text):
-    # client = Elasticsearch()
-    # s = Search(client)
-    # s.query('match', title=text)
-    # response = s.execute()
-    # return response.to_dict()
-    data = {
-        "query": {
-            "query_string": {"query": text}
-        }
-    }
-
-    response = requests.post('http://127.0.0.1:9200/lesson_index/lesson/_search', data=json.dumps(data))
-
-    hits = response.json()['hits']['hits']
-    resp = list()
-    for hit in hits:
-        l = dict()
-        l['id'] = hit['_id']
-        l['title'] = hit['_source']['title']
-        resp.append(l)
-    return resp
+    s = S(LessonMapping)
+    return s.query(title__match=text)
