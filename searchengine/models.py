a14d738a23694416a269ab629b3c4782762257e4
taery <lavrenko.a@gmail.com>
2015-07-31 18:03:29 +0300

    add mapping for lesson type

--- searchengine/models.py
+++ searchengine/models.py
@@ -1,10 +1,42 @@
 from django.db import models
-# from elasticsearch import Elasticsearch
-
+from elasticutils.contrib.django import MappingType, Indexable
 
 class Lesson(models.Model):
     id = models.BigIntegerField(primary_key=True)
     title = models.CharField(max_length=255)
 
     def __str__(self):
-        return str(self.id) + ' - ' + self.title
+        return str(self.id) + ' - ' + str(self.title)
+
+
+class LessonMapping(MappingType, Indexable):
+    @classmethod
+    def get_index(cls):
+        return 'lesson_index'
+
+    @classmethod
+    def get_mapping_type_name(cls):
+        return 'lesson-entry-type'
+
+    @classmethod
+    def extract_document(cls, obj_id, obj=None):
+        if obj is None:
+            obj = cls.get_model().objects.get(pk=obj_id)
+        return {
+            'id': obj.id,
+            'title': obj.title
+        }
+
+    @classmethod
+    def get_model(cls):
+        return Lesson
+
+    def get_object(self):
+        return self.get_model().objects.get(pk=self._id)
+
+    @classmethod
+    def get_mapping(cls):
+        return {
+            'pk': {'type': 'integer'},
+            'title': {'type': 'string'}
+        }
