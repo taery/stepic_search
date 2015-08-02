beabcce1d75f21c0034e9f2d469cdee1fd1f43fc
Anastasia Lavrenko <lavrenko.a@gmail.com>
2015-08-02 18:51:44 +0300

    Add styles

--- searchengine/forms.py
+++ searchengine/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    search_text = forms.CharField(label='Stepic search', required=False)
+    search_text = forms.CharField(label="", required=False)
+    search_text.widget.attrs.update({'autofocus': 'true'})
--- stepic_search/settings.py
+++ stepic_search/settings.py
@@ -30,15 +30,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = (
+    'rest_framework',
     'searchengine',
+    'django.contrib.staticfiles',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'rest_framework',
 )
 
 
